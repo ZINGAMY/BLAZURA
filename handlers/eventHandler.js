1375efc87cc2b92e674dec50d6d3c7c49d770cfb
ZINGAMY <z1ngam6.multicraft@gmail.com>
2025-10-23 18:43:10 +0530

    Refactor event loading mechanism in eventHandler.js

--- handlers/eventHandler.js
+++ handlers/eventHandler.js
@@ -1,54 +1,19 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
 module.exports = (client) => {
-  const eventsPath = path.join(__dirname, '../events');
-  let count = 0;
-
-  const loadEvents = (dir) => {
-    const files = fs.readdirSync(dir);
-
-    files.forEach((file) => {
-      const filePath = path.join(dir, file);
-      const stat = fs.lstatSync(filePath);
-
-      if (stat.isDirectory()) {
-        if (file === 'lavalink') {
-          const lavalinkFiles = fs.readdirSync(filePath);
-          lavalinkFiles.forEach((lavalinkFile) => {
-            if (lavalinkFile.endsWith('.js')) {
-              const event = require(path.join(filePath, lavalinkFile));
-
-              if (event.isNodeEvent) {
-                client.lavalink.nodeManager.on(event.name, (...args) =>
-                  event.execute(client, ...args)
-                );
-              } else {
-                client.lavalink.on(event.name, (...args) =>
-                  event.execute(client, ...args)
-                );
-              }
-              count++;
-            }
-          });
-        } else {
-          loadEvents(filePath);
-        }
-      } else if (file.endsWith('.js')) {
-        const event = require(filePath);
-        if (event.once) {
-          client.once(event.name, (...args) => event.execute(...args));
-        } else {
-          client.on(event.name, (...args) => event.execute(...args));
-        }
-        count++;
+  const eventFolders = fs.readdirSync(path.join(__dirname, "../events"));
+  for (const folder of eventFolders) {
+    const eventFiles = fs
+      .readdirSync(path.join(__dirname, `../events/${folder}`))
+      .filter((file) => file.endsWith(".js"));
+    for (const file of eventFiles) {
+      const event = require(`../events/${folder}/${file}`);
+      if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args, client));
+      } else {
+        client.on(event.name, (...args) => event.execute(...args, client));
       }
-    });
-  };
-
-  loadEvents(eventsPath);
-
-  console.log(
-    global.styles.successColor(`✅ Successfully loaded ${count} events`)
-  );
+    }
+  }
 };

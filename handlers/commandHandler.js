7a1b27329a224a13ae1ef9eb5e9008ab75d7eed3
ZINGAMY <z1ngam6.multicraft@gmail.com>
2025-10-23 18:44:25 +0530

    Simplify command loading in commandHandler.js
    Refactor command loading logic to simplify command registration.

--- handlers/commandHandler.js
+++ handlers/commandHandler.js
@@ -1,43 +1,13 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
+
 module.exports = (client) => {
-  client.commands = new Map();
+  const commandFiles = fs
+    .readdirSync(path.join(__dirname, "../commands"))
+    .filter((file) => file.endsWith(".js"));
 
-  const commandsPath = path.join(__dirname, '../commands');
-  fs.readdirSync(commandsPath).forEach((category) => {
-    const categoryPath = path.join(commandsPath, category);
-    const commandFiles = fs
-      .readdirSync(categoryPath)
-      .filter((file) => file.endsWith('.js'));
-
-    for (const file of commandFiles) {
-      const command = require(path.join(categoryPath, file));
-
-      client.commands.set(command.data.name, { ...command, category });
-    }
-  });
-  let commandCount = 0;
-  let categoryCount = 0;
-
-  const categories = fs.readdirSync(commandsPath);
-  categoryCount = categories.length;
-
-  categories.forEach((category) => {
-    const categoryPath = path.join(commandsPath, category);
-    const commandFiles = fs
-      .readdirSync(categoryPath)
-      .filter((file) => file.endsWith('.js'));
-
-    for (const file of commandFiles) {
-      const command = require(path.join(categoryPath, file));
-      client.commands.set(command.data.name, { ...command, category });
-      commandCount++;
-    }
-  });
-
-  console.log(
-    global.styles.successColor(
-      `✅ Loaded ${commandCount} commands across ${categoryCount} categories.`
-    )
-  );
+  for (const file of commandFiles) {
+    const command = require(`../commands/${file}`);
+    client.commands.set(command.data.name, command);
+  }
 };
